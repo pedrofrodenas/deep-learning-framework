71d7c9855a2b9fe81e0cf1ffcd1d0a2da2b645c2
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-02-06 11:43:47 +0100

    Resnet definition

--- src/models/myresnet.py
+++ src/models/myresnet.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 def resnet_head():
     conv_op = nn.Sequential(
@@ -9,21 +10,36 @@ def resnet_head():
     )
     return conv_op
 
-def resnet_block():
-    conv_op = nn.Sequential(
-        nn.Conv2d(64, 64, kernel_size=3, padding=1, stride = 1 ,bias=False),
-        nn.BatchNorm2d(64, eps=1e-05, momentum=0.1),
-        nn.ReLU(inplace=True),
-        nn.Conv2d(64, 64, kernel_size=3, padding=1, stride = 1 ,bias=False),
-        nn.BatchNorm2d(64, eps=1e-05, momentum=0.1),
-    )
-    return conv_op
-
 def resnet_bottleneck():
     conv_op = nn.Sequential(
         nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
     )
     return conv_op
+
+class BasicBlock(nn.Module):
+    def __init__(self, 
+                 input_channel: int,
+                 output_channel: int
+                ):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(input_channel, output_channel, kernel_size=3, padding=1, stride = 1 ,bias=False)
+        self.bn1 = nn.BatchNorm2d(output_channel, eps=1e-05, momentum=0.1)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(output_channel, output_channel, kernel_size=3, padding=1, stride = 1 ,bias=False)
+        self.bn2 = nn.BatchNorm2d(output_channel, eps=1e-05, momentum=0.1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        identity = x
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out += identity
+        out = self.relu(out)
+        return out
+
 
 class MyResNet32(nn.Module):
     def __init__(self, **kwargs):
@@ -34,15 +50,14 @@ class MyResNet32(nn.Module):
         # Resolution divided by 2
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
-        self.resnet_block = resnet_block()
+        self.layer1 = BasicBlock(64, 64)
     
     def forward(self, x):
 
         intro1 = self.intro_block(x)
         max1 = self.maxpool(intro1)
-        layer1 = self.resnet_block(max1)
-        layer2 = self.resnet_block(layer1)
+        layer1 = self.layer1(max1)
 
-        return layer2
+        return layer1
         
 
