0f7c3289456d3e3a8ff3da42d1507524f77a4967
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-02-06 10:35:46 +0100

    myresnet modifications

--- src/models/myresnet.py
+++ src/models/myresnet.py
@@ -9,10 +9,39 @@ def resnet_head():
     )
     return conv_op
 
+def resnet_block():
+    conv_op = nn.Sequential(
+        nn.Conv2d(64, 64, kernel_size=3, padding=1, stride = 1 ,bias=False),
+        nn.BatchNorm2d(64, eps=1e-05, momentum=0.1),
+        nn.ReLU(inplace=True),
+        nn.Conv2d(64, 64, kernel_size=3, padding=1, stride = 1 ,bias=False),
+        nn.BatchNorm2d(64, eps=1e-05, momentum=0.1),
+    )
+    return conv_op
+
+def resnet_bottleneck():
+    conv_op = nn.Sequential(
+        nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
+    )
+    return conv_op
+
 class MyResNet32(nn.Module):
     def __init__(self):
         super(MyResNet32, self).__init__()
 
         self.intro_block = resnet_head(3, 64)
+
+        # Resolution divided by 2
+        self.max_pooling_intro = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = nn.Sequential(resnet_block, resnet_block)
+    
+    def forward(self, x):
+
+        intro1 = self.intro_block(x)
+        max1 = self.max_pooling_intro(intro1)
+        layer1 = self.layer1(max1)
+
+        return layer1
         
 
