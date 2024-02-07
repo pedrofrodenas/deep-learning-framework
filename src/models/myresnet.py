70a76cb4710a34ce2474be9b685fd5f7eb244a61
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-02-07 14:23:33 +0100

    Updated resnet

--- src/models/myresnet.py
+++ src/models/myresnet.py
@@ -54,8 +54,6 @@ class DownsamplingBlock(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = nn.Conv2d(output_channel, output_channel, kernel_size=3, padding=1, stride = 1 ,bias=False)
         self.bn2 = nn.BatchNorm2d(output_channel, eps=1e-05, momentum=0.1)
-        # self.conv3_down = nn.Conv2d(input_channel, output_channel, kernel_size=1, padding=1, stride = 2 ,bias=False)
-        # self.bn3 = nn.BatchNorm2d(output_channel, eps=1e-05, momentum=0.1)
         self.downsampling = resnet_downsampling(input_channel, output_channel)
 
     def forward(self, x: Tensor) -> Tensor:
@@ -71,25 +69,34 @@ class DownsamplingBlock(nn.Module):
 
 
 class MyResNet18(nn.Module):
-    def __init__(self, **kwargs):
+    def __init__(self,
+                 classes: int = 1000,
+                 **kwargs):
         super(MyResNet18, self).__init__()
 
         self.intro_block = resnet_head()
 
         # Resolution divided by 2
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-
-        # self.layer1 = BasicBlock(64, 64)
         self.layer1 = nn.Sequential(BasicBlock(64, 64), BasicBlock(64, 64))
-
         self.layer2 = nn.Sequential(DownsamplingBlock(64, 128), BasicBlock(128, 128))
+        self.layer3 = nn.Sequential(DownsamplingBlock(128, 256), BasicBlock(256, 256))
+        self.layer4 = nn.Sequential(DownsamplingBlock(256, 512), BasicBlock(512, 512))
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512 , classes)
     
-    def forward(self, x):
+    def forward(self, x: Tensor) -> Tensor:
 
-        intro1 = self.intro_block(x)
-        max1 = self.maxpool(intro1)
-        layer1 = self.layer1(max1)
+        x = self.intro_block(x)
+        x = self.maxpool(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
 
-        return layer1
+        return x
         
 
