be847e387c61a902b114c796242dd20e4a0ec3f2
Pedro Ródenas <pedro.rodenas@gmail.com>
2024-02-08 09:18:16 +0100

    Added last modules

--- src/models/myresnet.py
+++ src/models/myresnet.py
@@ -71,6 +71,7 @@ class DownsamplingBlock(nn.Module):
 class MyResNet18(nn.Module):
     def __init__(self,
                  classes: int = 1000,
+                 zero_init_residual: bool = False,
                  **kwargs):
         super(MyResNet18, self).__init__()
 
@@ -84,6 +85,28 @@ class MyResNet18(nn.Module):
         self.layer4 = nn.Sequential(DownsamplingBlock(256, 512), BasicBlock(512, 512))
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 , classes)
+
+        # Weights Initialization
+        # fan_out because I assume that if you suspect your backward pass 
+        # might be more "chaotic" (greater variance) it is worth changing 
+        # the mode to fan_out. This might happen when the loss oscillates 
+        # a lot (e.g. very easy examples followed by very hard ones).
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+        # Zero-initialize the last BN in each residual branch,
+        # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+        # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, DownsamplingBlock) and m.downsampling[1].weight is not None:
+                    nn.init.constant_(m.bn3.weight, 0)  # type: ignore[arg-type]
+                elif isinstance(m, BasicBlock) and m.bn2.weight is not None:
+                    nn.init.constant_(m.bn2.weight, 0)  # type: ignore[arg-type]
     
     def forward(self, x: Tensor) -> Tensor:
 
