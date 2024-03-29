d505449871c0ae658c24d1759d6869cb1ef8b3ca
pedrofrodenas <pedrofrodenas@gmail.com>
2024-03-29 12:32:22 +0100

    Added weight initialization

--- src/models/mymobilenetv2.py
+++ src/models/mymobilenetv2.py
@@ -7,6 +7,8 @@ from torchvision.ops.misc import Conv2dNormActivation
 
 from typing import List
 
+from utils import make_divisible
+
 class InvertedResidual(nn.Module):
     def __init__(self,
                  inp,
@@ -63,7 +65,90 @@ class InvertedResidual(nn.Module):
 
 
 class MyMobilenetv2(nn.Module):
-    def __init__(self):
+    def __init__(self,
+                 num_classes: int = 1000,
+                 width_mult: float = 1.0,
+                 dropout: float = 0.2):
         super(MyMobilenetv2, self).__init__()
 
-        input_channel = 32
+        first_layer_output = 32
+        last_layer_output = 1280
+
+        # Table2 Mobilenetv2 paper
+        settings = [
+                # t, c, n, s
+                [1, 16, 1, 1],
+                [6, 24, 2, 2],
+                [6, 32, 3, 2],
+                [6, 64, 4, 2],
+                [6, 96, 3, 1],
+                [6, 160, 3, 2],
+                [6, 320, 1, 1],
+            ]
+
+        features: List[nn.Module] = []
+
+        # Width multiplier can reduce or increase the number of
+        # input channels but it is necessary to be multiple
+        # of 8
+        first_layer_output = make_divisible(first_layer_output * width_mult, 8)
+        # Also applied to the last layer
+        self.last_layer_output = make_divisible(last_layer_output * width_mult, 8)
+
+        # First layer, downsampling / 2
+        features.append(Conv2dNormActivation(
+            3,
+            first_layer_output,
+            kernel_size=3,
+            stride=2,
+            activation_layer=nn.ReLU6
+        ))
+
+        for t, c, n, s in settings:
+
+            output_mod = make_divisible(c * width_mult, 8)
+            # Each pattern is repeated n times, with n=0 stride=s and
+            # downsampling to spatial dimension is applied, otherwise
+            # stride = 1
+            for i in range(n):
+                stride = s if i == 0 else 1
+                features.append(InvertedResidual(first_layer_output, 
+                                                 output_mod,
+                                                 stride=stride,
+                                                 expand_ratio=t))
+                first_layer_output = output_mod
+
+        # Append last layer t = 1, c = 1280, n = 1, s = 1
+        features.append(Conv2dNormActivation(first_layer_output,
+                                             self.last_layer_output,
+                                             kernel_size=1,
+                                             stride=1,
+                                             activation_layer=nn.ReLU6))
+        
+        self.features = nn.Sequential(*features)
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(p=dropout),
+            nn.Linear(self.last_layer_output, num_classes),
+        )
+
+        # weight initialization
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.ones_(m.weight)
+                nn.init.zeros_(m.bias)
+            elif isinstance(m, nn.Linear):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+
+
+        
+
+            
+
+
